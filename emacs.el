c0d0e669abd65656ee51b4d15f0016b41b67241d
David de Klerk <dawiedotcom@gmail.com>
2013-05-30 14:15:14 +0200

    Update emacs config

--- emacs.el
+++ emacs.el
@@ -8,16 +8,14 @@
 ;;; Always do syntax highlighting
 ;(global-font-lock-mode 1)
 ;
-;;;; Also highlight parens
-;(setq show-paren-delay 0
-;      show-paren-style 'parenthesis)
-;(show-paren-mode 1)
-
-;;;; Setup packages
+;;;; Install packages
 
 (require 'package)
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("org" . "http://orgmode.org/elpa/")
+	     t)
 (package-initialize)
 
 (defvar packages
@@ -25,7 +23,9 @@
     clojure-test-mode
     nrepl
     evil
-    quack))
+    quack
+    org
+    diff-hl))
 
 (defun install-missing-packages-p (ps)
   (cond ((not ps) nil)
@@ -39,10 +39,12 @@
   (package-refresh-contents)
   (dolist (p packages)
     (when (not (package-installed-p p))
-      (package-install p))))
+      (package-install p)))
+  (package-initialize))
 
-;;;; Setup evil
+;;;; Setup Packages
 
+;; Evil
 (add-to-list 'load-path "~/.emacs.d/elpa/evil-1.0.2")
 (require 'evil)
 (evil-mode 1)
@@ -51,12 +53,53 @@
 (define-key evil-insert-state-map "\C-y" 'yank)
 (define-key evil-visual-state-map "\C-y" 'yank)
 
-;;;; This is the binary name of my scheme implementation
+;; Quack - for scheme
 ;(setq scheme-program-name "scheme-repl")
 (require 'quack)
 
-;; Enable line numbers 
-(global-linum-mode t)
+;; Install matlab-emacs
+;(add-to-list 'load-path "~/.emacs.d/matlab-emacs")
+;(load-library "matlab-load")
 
-;; Indent on return
+;; Org mode
+;;  http://orgmode.org
+(add-hook 'org-mode-hook 'turn-on-front-lock)
+(global-set-key "\C-c1" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;; diff-hl
+(global-diff-hl-mode)
+
+;;;; Settings
+
+(global-linum-mode t)
 (define-key global-map (kbd "RET") 'newline-and-indent)
+(tool-bar-mode -1)
+(load-theme 'zenburn t)
+(setq inhibit-startup-message t)	
+(setq tabwidth 4)
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'cperl-indent-level 'tab-width)
+
+;; iswitch buffers C-x b - http://www.emacswiki.org/emacs/IswitchBuffers
+(iswitchb-mode 1)
+
+;; Highlight parens - http://www.emacswiki.org/emacs/ShowParenMode
+(setq show-paren-delay 0
+      show-paren-style 'parenthesis)
+(show-paren-mode 1)
+
+;(custom-set-variables
+; ;; custom-set-variables was added by Custom.
+; ;; If you edit it by hand, you could mess it up, so be careful.
+; ;; Your init file should contain only one such instance.
+; ;; If there is more than one, they won't work right.
+; '(custom-safe-themes (quote ("9f443833deb3412a34d2d2c912247349d4bd1b09e0f5eaba11a3ea7872892000" default)))
+; '(tool-bar-mode nil))
+;(custom-set-faces
+; ;; custom-set-faces was added by Custom.
+; ;; If you edit it by hand, you could mess it up, so be careful.
+; ;; Your init file should contain only one such instance.
+; ;; If there is more than one, they won't work right.
+; )
