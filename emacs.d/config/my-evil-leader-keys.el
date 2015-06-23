61deae5ec83d922194f793e0706741051920a9ad
David de Klerk <dawiedotcom@gmail.com>
2015-06-23 12:10:37 +0200

    Add emacs language modes for Rust, Lua and Matlab and Magit.

--- emacs.d/config/my-evil-leader-keys.el
+++ emacs.d/config/my-evil-leader-keys.el
@@ -1,6 +1,14 @@
 
+;; <leader> keybindings
+
 (evil-leader/set-key
- "," 'ibuffer)
+ "," 'ibuffer
+ ;; Magit shortcuts
+ "gs" 'magit-status
+ "gl" 'magit-log
+ "gd" 'magit-diff
+ ;; Toggle goto-address-mode
+ "m" 'goto-address-mode)
 
 
 (provide 'my-evil-leader-keys)
---
+++ emacs.d/config/my-lua.el
@@ -0,0 +1,7 @@
+
+
+(use-package lua-mode
+  ;:mode ("\\.lua\\'" . lua-mode)
+  :ensure lua-mode)
+
+(provide 'my-lua)
